dd4cec3bdd2016ea68830e941f729ca9dabcda75
Francisco Javier Hidalgo Frias <friasjavi45@gmail.com>
2018-11-11 13:58:02 +0100

    Fragment09 - Paso1

---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment09/Fragment09.java
@@ -0,0 +1,28 @@
+package com.iesvirgendelcarmen.dam.fragment09;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class Fragment09 extends FragmentActivity {
+    Adaptador paginador;
+    ViewPager visor;
+    ArrayList<Fragment> listaFragmentos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment09);
+
+        listaFragmentos = new ArrayList<Fragment>();
+        listaFragmentos.add(new Fragmento1());
+        listaFragmentos.add(new Fragmento2());
+        paginador = new Adaptador(getSupportFragmentManager(),listaFragmentos);
+        visor = (ViewPager) findViewById(R.id.viewPager);
+        visor.setAdapter(paginador);
+    }
+}
